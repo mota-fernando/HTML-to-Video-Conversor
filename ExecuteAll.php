ad5b91e1c2a768508499e4f8fa70ff1a9e43836b
mota-fernando <motafn+github@gmail.com>
2018-09-21 11:56:07 -0300

    Removed .bak files.

---
+++ ExecuteAll.php
@@ -0,0 +1,9 @@
+<?php
+/*Get content from website*/
+$GetContent = new GetContent;
+//$GetContent->Test();
+$GetContent->InsertIntoDB(); 
+
+/*Gererating a video*/
+$Html2video = new Html2video;
+
---
+++ Html2video.php
@@ -0,0 +1,19 @@
+<meta content="UTF-8">
+<?php
+/*
+@Author: Fernando Mota
+@Licend: MIT
+*/
+class Html2video{
+	
+	private function Animate($element,$property){}
+	private function Transition($element,$property){}
+	private function Effect($element, $property){}
+	private function Speech($element, $property){}
+	private function BackgroundNoise ($element, $property)
+	private function LoadSettings($settingFile){}
+	private function Write($text,$property);
+	private function LoadHtml($html);
+	private function Render(){return $outputURL;}
+}
+?>
